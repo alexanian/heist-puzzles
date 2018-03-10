d174ad209cd1d909ea3c3b72b26571a831a51a63
tessa <tessa@zymergen.com>
2018-03-10 14:27:02 -0800

    Laser uses digitalRead to track pin state
    ... As it should have all along. Oh well.

--- laser-maze/photon_code/laser-blinks.ino
+++ laser-maze/photon_code/laser-blinks.ino
@@ -10,9 +10,6 @@
 int numSwitches = 3;
 int switches[] = { D0, D1, D2 };
 
-// Track whether switches are on
-bool isSwitchOn[] = { false, false, false };
-
 // How long should each laser stay on for?
 int delayTime = 1000;
 
@@ -32,63 +29,58 @@ void loop() {
 
 
 /**
-  Toggles a pin value from low to high.
-
-  @param pinIsHigh bool indicating current pin value. Inverted when pin is written.
+  Toggles a pin value between low and high.
 */
-void togglePin(int pin, bool &pinIsHigh) {
-    if (pinIsHigh) {
+void togglePin(int pin) {
+    bool isPinHigh = digitalRead(pin);
+    
+    if (isPinHigh) {
         digitalWrite(pin,LOW);
-        pinIsHigh = false;
     }
     else {
         digitalWrite(pin,HIGH);
-        pinIsHigh = true;
     }
 }
 
 /**
   Rotate having a single switch off.
-  
-  @param blinkTime milliseconds to keep each switch will be off for.
 */
 void rotateBlink(int blinkTime) {
     
     // Setup: turn off and other two on
     digitalWrite(switches[0],LOW);
-    isSwitchOn[0] = false;
-    
     digitalWrite(switches[1],HIGH);
-    isSwitchOn[1] = true;
-    
     digitalWrite(switches[2],HIGH);
-    isSwitchOn[2] = true;
     
     publishSwitchState();
     
     // Turn one off, turn zero on
-    togglePin(switches[1], isSwitchOn[1]);
-    togglePin(switches[0], isSwitchOn[0]);
+    togglePin(switches[1]);
+    togglePin(switches[0]);
 
     publishSwitchState();
     delay(blinkTime);
     
     // Turn two off, turn one on
-    togglePin(switches[2], isSwitchOn[2]);
-    togglePin(switches[1], isSwitchOn[1]);
+    togglePin(switches[2]);
+    togglePin(switches[1]);
     
     publishSwitchState();
     delay(blinkTime);
 }
 
-/**
-  Publish a "switchState" event, i.e. if only switch two is on "010".
-  Useful for debugging.
-*/
+
 void publishSwitchState() {
     sprintf(publishString, "%d %d %d",
         digitalRead(switches[0]),
         digitalRead(switches[1]),
         digitalRead(switches[2]));
     Particle.publish("switchState", publishString, 60, PRIVATE);
+}
+
+
+/**
+  Flash lasers very quickly. PANIC!!!
+*/
+void panic() {
 }
