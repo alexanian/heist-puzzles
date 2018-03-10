aa9e7679bec11dacc55b600f3bee73c46e0ec43d
tessa <tessa@zymergen.com>
2018-03-09 23:11:18 -0800

    Refactored laser-blinks
    Removed code duplication, much more readable
    Also pulled out photoresistor read events, since not currently using
    them.
    
    Next steps: program some different laser patterns.

--- laser-maze/photon_code/laser-blinks.ino
+++ laser-maze/photon_code/laser-blinks.ino
@@ -1,108 +1,57 @@
 // -----------------------------------------
 //  Blink lasers according to API timeouts 
 // -----------------------------------------
-// This is basically the first half of the laser mase. 
+// This is basically the first half of the laser maze. 
 
 
 // Declare pin variables
-int switch1 = D0; // Signal for a MOSFET connected to Laser 1
-int switch2 = D1; // Signal for a MOSFET connected to Laser 2 and Laser 3
-int switch3 = D2; // Signal for a MOSFET connected to Laser 4 and Laser 5
 
-int photoresistor = A1;
+// Each switch is a signal for a MOSFET connected to 1 or 2 lasers
+int numSwitches = 3;
+int switches[] = { D0, D1, D2 };
 
+// Track whether switches are on
+bool isSwitchOn[] = { false, false, false };
 
-// Booleans to indicate current status of each laser
-bool isLaser1 = false;
-bool isLaser2 = false;
-bool isLaser3 = false;
-bool isLaser4 = false;
-bool isLaser5 = false;
-
-char publishString[40];
-int read;
+// How long should each laser stay on for?
+int delayTime = 1000;
 
 void setup() {
     // All switches are output
-    pinMode(switch1,OUTPUT);
-    pinMode(switch2,OUTPUT);
-    pinMode(switch3,OUTPUT);
-    
-    digitalWrite(switch1,HIGH);
-    digitalWrite(switch2,HIGH);
-    digitalWrite(switch3,HIGH);
+	for (int ii = 0; ii < numSwitches; ii++) {
+		pinMode(switches[ii],OUTPUT);
+	}
 }
 
 void loop() {
-    toggleSwitchOne();
-    read = analogRead(photoresistor);
-    sprintf(publishString, "%d", read);
-    Particle.publish("beamMeasurement",publishString,60,PRIVATE);
-    delay(1000);
-    toggleSwitchOne();
-    read = analogRead(photoresistor);
-    sprintf(publishString, "%d", read);
-    Particle.publish("beamMeasurement",publishString,60,PRIVATE);
-    delay(1000);
-    toggleSwitchTwo();
-    read = analogRead(photoresistor);
-    sprintf(publishString, "%d", read);
-    Particle.publish("beamMeasurement",publishString,60,PRIVATE);
-    delay(1000);
-    toggleSwitchTwo();
-    read = analogRead(photoresistor);
-    sprintf(publishString, "%d", read);
-    Particle.publish("beamMeasurement",publishString,60,PRIVATE);
-    delay(1000);
-    toggleSwitchThree();
-    read = analogRead(photoresistor);
-    sprintf(publishString, "%d", read);
-    Particle.publish("beamMeasurement",publishString,60,PRIVATE);
-    delay(1000);
-    toggleSwitchThree();
-    read = analogRead(photoresistor);
-    sprintf(publishString, "%d", read);
-    Particle.publish("beamMeasurement",publishString,60,PRIVATE);
-    delay(1000);
+    togglePin(switches[0], isSwitchOn[0]);
+    delay(delayTime);
+	togglePin(switches[0], isSwitchOn[0]);
+    delay(delayTime);
+	togglePin(switches[1], isSwitchOn[1]);
+    delay(delayTime);
+    togglePin(switches[1], isSwitchOn[1]);
+    delay(delayTime);
+    togglePin(switches[2], isSwitchOn[2]);
+    delay(delayTime);
+    togglePin(switches[2], isSwitchOn[2]);
+    delay(delayTime);
 }
 
 
-void toggleSwitchOne()
-{
-    if (isLaser1) {
-        digitalWrite(switch1,LOW);
-        isLaser1 = false;
-    }
-    else {
-        digitalWrite(switch1,HIGH);
-        isLaser1 = true;
-    }
-}
+/**
+  Toggles a pin value from low to high based on a Boolean.
 
-void toggleSwitchTwo()
+  @param pinIsHigh bool indicating current pin value. Changed when pin is toggled.
+*/
+void togglePin(int pin, bool &pinIsHigh)
 {
-    if (isLaser2) {
-        digitalWrite(switch2,LOW);
-        isLaser2 = false;
-        isLaser3 = false;
+    if (pinIsHigh) {
+        digitalWrite(pin,LOW);
+        pinIsHigh = false;
     }
     else {
-        digitalWrite(switch2,HIGH);
-        isLaser2 = true;
-        isLaser3 = true;
-    }
-}
-
-void toggleSwitchThree()
-{
-    if (isLaser4) {
-        digitalWrite(switch3,LOW);
-        isLaser4 = false;
-        isLaser5 = false;
-    }
-    else {
-        digitalWrite(switch3,HIGH);
-        isLaser4 = true;
-        isLaser5 = true;
+        digitalWrite(pin,HIGH);
+        pinIsHigh = true;
     }
 }
