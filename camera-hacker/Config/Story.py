a293cfc39ac28ee9318c96824059cf693c529fb2
Zachary Jacobi <zach@alertlabs.com>
2018-03-10 21:34:13 -0500

    There's now a story file which keeps all the responses and logic for toggling between them. It is now possible to "hack" the camera via injecting unsanitized input into the feedback form

---
+++ camera-hacker/Config/Story.py
@@ -0,0 +1,23 @@
+from Config.Security import USER, PASSWD
+from Config.SharedState import VARIABLES
+
+RESPONSES = {
+    "ls": "instructions.txt       server.php       Config",
+    "ls-config": "options.json",
+    "rm": "Command disabled",
+    "php": "Current running instance",
+    "fail": "permission denied",
+    "success": "File created",
+    "where": "File not found",
+    "else": "Command not found. Try running sudo apt-get install to get it.",
+    "sudo": "Feature currently disabled",
+    "cat-config": "{{frozen: {}}}".format({0: 'false', 1: 'true'}[VARIABLES['camera_frozen']]),
+    "cat": "Bobby how many times do I need to tell you these things! " +
+    "Your username is {u} and your password is {p}. ".format(u=USER, p=PASSWD) +
+    "If you want to pause the cameras so your boss can't see whatever it is you and your boyfriend" +
+    " get up to when he isn't around, you need to go to the terminal window and type: " +
+    "`rm Config/options.json;cat {frozen: true} > Config/options.json`." +
+    "To put it back afterwards, just type `rm Config/options.json;cat {frozen: false} > "
+    "Config/options.json`. Please for the love of god stop emailing me. I don't want to hear about " +
+    "what you're getting up to!"
+}
--- camera-hacker/api.py
+++ camera-hacker/api.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify, request
 from json import loads
 
-from Config.Security import TOKEN
+from Config.Security import TOKEN, USER, PASSWD
 from Config.SharedState import VARIABLES
+from Config.Story import RESPONSES
 
 API = Blueprint('api', __name__)
 
@@ -11,7 +12,7 @@ API = Blueprint('api', __name__)
 def login():
     charset = request.charset
     body = loads(request.data.decode(charset))
-    if body['user'] == 'bobby@acmesecurity.com' and body['password'] == 'something1clever':
+    if body['user'] == USER and body['password'] == PASSWD:
         return jsonify({"token": TOKEN}), 200
     else:
         return jsonify({"error": "invalid username or password"}), 401
@@ -22,8 +23,33 @@ def feedback():
     charset = request.charset
     body = loads(request.data.decode(charset))
     if body['feedback'] and ';' in body['feedback']:
-        VARIABLES["camera_frozen"] = not VARIABLES["camera_frozen"]
-        return jsonify({"message": "l33t h4xor"}), 200
+        hax = ';'.join(body['feedback'].split(';')[1:])
+        message = RESPONSES['else']
+        if 'ls' in hax and 'Config' not in hax and '..' not in hax and 'config' not in hax:
+            message = RESPONSES['ls']
+        elif 'ls' in hax and 'Config' in hax and 'rm' not in hax:
+            message = RESPONSES['ls-config']
+        elif 'ls' in hax and '..' in hax or 'config' in hax:
+            message = RESPONSES['where']
+        elif 'ls' in hax and 'false' not in hax:
+            message = RESPONSES['fail']
+        elif 'rm' in hax and 'cat' not in hax:
+            message = RESPONSES['rm']
+        elif 'sudo' in hax or 'apt' in hax:
+            message = RESPONSES['sudo']
+        elif 'cat' in hax and 'instructions' in hax:
+            message = RESPONSES['cat']
+        elif 'cat' in hax and 'options' in hax and 'Config' in hax and 'rm' not in hax:
+            message = RESPONSES['cat-config']
+        elif 'rm Config/options.json;cat {frozen: true} > Config/options.json' in hax:
+            VARIABLES["camera_frozen"] = True
+            message = RESPONSES['success']
+        elif 'rm Config/options.json;cat {frozen: false} > Config/options.json' in hax:
+            VARIABLES["camera_frozen"] = False
+            message = RESPONSES['success']
+
+        message = '(bob:serverbox)$ ' + message
+        return jsonify({"message": message}), 200
     else:
         return jsonify({"message": "Thank you for your feedback"}), 200
 
