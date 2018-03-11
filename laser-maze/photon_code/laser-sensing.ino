5c3f73bb55d3af63a680dd710f31d1b03db0e40a
tessa <tessa@zymergen.com>
2018-03-11 14:45:48 -0700

    Tested sensing code
    Runs by blinking just a single laser
    
    Next steps: improve isBeamBroken to take into account blinking

--- laser-maze/photon_code/laser-sensing.ino
+++ laser-maze/photon_code/laser-sensing.ino
@@ -7,21 +7,18 @@
 // Declare pin variables
 
 // Each switch is a signal for a MOSFET connected to 1 or 2 lasers
-int numSwitches = 3;
-int switches[] = { D0, D1, D2 };
+const int numSwitches = 3;
+const int switches[numSwitches] = { D0, D1, D2 };
 
 // Each photoresistor detects the prescence or absence of a laser
-int numLasers = 5;
-int photoresistors[] = { A0, A1, A2, A3, A4 };
-int beamThresholds[5];
+const int numLasers = 5;
+const int photoresistors[numLasers] = { A0, A1, A2, A3, A4 };
+int beamThresholds[numLasers];
 
 // Controlling TIME
-int blinkPeriod = 1000; // How long should each laser stay on for?
+int blinkPeriod = 2000; // How long should each laser stay on for?
 int timeSinceBlink = 0;
-int loopTime = 10;
-
-// For publishing event strings!
-char publishString[40];
+int loopTime = 1000;
 
 void setup() {
     // All switches are output
@@ -34,22 +31,31 @@ void setup() {
 	for (int ii = 0; ii < numLasers; ii++) {
 	    beamThresholds[ii] = calibrateResistor(photoresistors[ii]);
 	}
+	
+	togglePin(switches[0]);
 }
 
 void loop() {
+    char timeString[40];
+    sprintf(timeString, "time since blink: %d", timeSinceBlink);
+    Particle.publish("timeSinceBlink",timeString,60,PRIVATE);
+    
     publishResistorReadings();
+    
+    if (timeSinceBlink >= blinkPeriod) {
+        togglePin(switches[0]);
+        timeSinceBlink = 0;
+    }
     
     if(isBeamBroken()) {
         Particle.publish("alert","beam is broken!",60,PRIVATE);
     }
-    else if (timeSinceBlink >= blinkPeriod) {
-        // switch to next state
-        togglePin(switches[1]);
-        timeSinceBlink = 0;
-    }
+
+    digitalWrite(led, LOW);
 
     timeSinceBlink += loopTime;
     delay(loopTime);
+    digitalWrite(led, HIGH);
 }
 
 
@@ -65,7 +71,7 @@ int calibrateResistor(int pin) {
 */
 bool isBeamBroken() {
     bool isBeamBroken = false;
-	for (int ii = 0; ii < numLasers; ii++) {
+	for (int ii = 0; ii < 1; ii++) {
 	    int beamValue = analogRead(photoresistors[ii]);
 	    isBeamBroken = isBeamBroken || beamValue < beamThresholds[ii]; 
 	}
@@ -90,21 +96,15 @@ void togglePin(int pin) {
 }
 
 /**
-  Publish readings for each resistor
+  Publish a beamMeasurement event with the analog values of each photoresistor.
  */
 void publishResistorReadings() {
-	for (int ii = 0; ii < numLasers; ii++) {
-	    publishResistorReading(photoresistors[ii]);
+    char publishString[128] = "";
+    
+    for (int ii = 0; ii < numLasers; ii++) {
+        int pin = photoresistors[ii];
+        sprintf(publishString + strlen(publishString), "pin %d: %d, ", pin, analogRead(pin));
 	}
-}
-
-/**
-  Publish a beamMeasurement event with the analog value of a pin.
-  
-  @param pin integer id of the pin to be read.
- */
-void publishResistorReading(int pin) {
-    int read = analogRead(pin);
-    sprintf(publishString, "pin %d has value: %d", pin, read);
-    Particle.publish("beamMeasurement",publishString,60,PRIVATE);
+    
+    Particle.publish("beamMeasurement", publishString, 60, PRIVATE);
 }
