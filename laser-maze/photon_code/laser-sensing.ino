9e3a030b3bfd2792d7668a97c50f1362af9acc75
tessa <tessa@zymergen.com>
2018-03-11 16:13:21 -0700

    Laser sensing with calibration

--- laser-maze/photon_code/laser-sensing.ino
+++ laser-maze/photon_code/laser-sensing.ino
@@ -11,15 +11,15 @@ const int numSwitches = 3;
 const int switches[numSwitches] = { D0, D1, D2 };
 
 // Each photoresistor detects the prescence or absence of a laser
-const int numLasers = 5;
-const int photoresistors[numLasers] = { A0, A1, A2, A3, A4 };
-const int laserSwitches[numLasers] = { D0, D1, D1, D2, D2 }; // some are doubled up
+const int numLasers = 2;//5;
+const int photoresistors[numLasers] = { A0, A1 };//, A2, A3, A4 };
+const int laserSwitches[numLasers] = { D0, D1 };//, D1, D2, D2 }; // some are doubled up
 int beamThresholds[numLasers];
 
 // Controlling TIME
-int blinkPeriod = 2000; // How long should each laser stay on for?
+int blinkPeriod = 1000; // How long should each laser stay on for?
 int timeSinceBlink = 0;
-int loopTime = 1000;
+int loopTime = 500;
 
 void setup() {
     // All switches are output
@@ -30,47 +30,75 @@ void setup() {
 	
 	// Calibrate photoresistors
 	for (int ii = 0; ii < numLasers; ii++) {
-	    beamThresholds[ii] = calibrateResistor(photoresistors[ii]);
+	    beamThresholds[ii] = calibrateResistor(photoresistors[ii], laserSwitches[ii]);
 	}
 	
-	togglePin(switches[0]);
+    char publishString[128] = "";
+    for (int ii = 0; ii < numLasers; ii++) {
+        int pin = photoresistors[ii];
+        sprintf(publishString + strlen(publishString), "beam %d: %d, ", pin, beamThresholds[ii]);
+	}
+    Particle.publish("beamThreshold", publishString, 60, PRIVATE);
+	
+	togglePin(switches[2]);
 }
 
 void loop() {
     publishResistorReadings();
     
-    if (timeSinceBlink >= blinkPeriod) {
-        togglePin(switches[0]);
-        timeSinceBlink = 0;
-    }
-    
     if(isBeamBroken()) {
         Particle.publish("alert","beam is broken!",60,PRIVATE);
     }
-
+    else if (timeSinceBlink >= blinkPeriod) {
+        togglePin(switches[2]);
+    	togglePin(switches[1]);
+        timeSinceBlink = 0;
+    }
+    
     timeSinceBlink += loopTime;
     delay(loopTime);
 }
 
 
 /**
-  Right now, set the beam threshold for all lasers at 3800.
-  This could be fancier... eventually.
+  Calibrate photoresistors- necessary because ambient light levels change.
+  
+  @param photoresistor integer id of photoresistor to read.
+  @param laserSwitch integer id of switch for laser pointed at photoresistor.
  */
-int calibrateResistor(int pin) {
-    return 3800;
+int calibrateResistor(int photoresistor, int laserSwitch) {
+    // Average two measurements while the laser is on
+    digitalWrite(laserSwitch, HIGH);
+    int on_1 = analogRead(photoresistor);
+    delay(1000);
+    int on_2 = analogRead(photoresistor);
+    delay(1000);
+    int on_3 = analogRead(photoresistor);
+    int on = (on_1 + on_2 + on_3) / 3;
+    
+    // Average two measurements while the laser if off
+    digitalWrite(laserSwitch, LOW);
+    int off_1 = analogRead(photoresistor);
+    delay(1000);
+    int off_2 = analogRead(photoresistor);
+    delay(1000);
+    int off_3 = analogRead(photoresistor);
+    int off = (off_1 + off_2 + off_3) / 3;
+    
+    // Calibrate the beam threshold to be halfway between the 
+    return (on*2 + off)/3;
 }
 
 /**
-  Return true if any of the beams are broken
+  Return true if any of the beams are broken.
 */
 bool isBeamBroken() {
     bool isBeamBroken = false;
-	for (int ii = 0; ii < 1; ii++) {
+	for (int ii = 0; ii < numLasers; ii++) {
 	    if (laserSwitchedOff(ii)) continue;
 	    
 	    int beamValue = analogRead(photoresistors[ii]);
-	    isBeamBroken = isBeamBroken || beamValue < beamThresholds[ii]; 
+	    isBeamBroken = isBeamBroken || ( beamValue < beamThresholds[ii] ); 
 	}
 	return isBeamBroken;
 }
