e0e29cb138e6c616741174f4ca2cdc4dfc47f476
tessa <tessa@zymergen.com>
2018-03-15 21:46:22 -0700

    First legitimately functional security interface!

--- laser-maze/webapp/events.js
+++ laser-maze/webapp/events.js
@@ -1,39 +1,75 @@
-// Particle event detection, borrowed from this tutorial:
+// Security system button and Particle device events
+//
+// Based particle on the Particle event detection tutorial:
 // https://community.particle.io/t/tutorial-getting-started-with-spark-publish/3422
-// accessToken should be imported from another (secret) file
-var secondsElapsed = 0;
+var secondsSinceAlert = 0;
+var inAlertMode = true;
 
-function start() {
-    var activeSpan = document.getElementById("is-active");
-    activeSpan.innerHTML = "active";
+// accessToken should be includes in secrets.js, which is not versioned
+var deviceURL = "https://api.spark.io/v1/devices/" + deviceId + "/";
+var eventsURL = "https://api.spark.io/v1/devices/events/?access_token=" + accessToken;
 
-    // var eventSource = new EventSource("https://api.spark.io/v1/devices/events/?access_token=" + accessToken);
-    // eventSource.addEventListener('alert', function(e) {
-    //     var parsedData = JSON.parse(e.data);
-    //     var tempSpan = document.getElementById("status");
-    //     tempSpan.innerHTML = "Status: " + parsedData.data;
-    //     tempSpan.style.fontSize = "28px";
-        
-    //     var tsSpan = document.getElementById("ts");
-    //     tsSpan.innerHTML = "At timestamp " + parsedData.published_at;
-    //     tsSpan.style.fontSize = "9px";
-    // }, false);
+// Begin monitoring the event sources and updating the timespace
+$(document).ready( function() {
+    // Set up events
+    var eventSource = new EventSource(eventsURL);
+    eventSource.addEventListener('alert', function (e) {
+        alert();
+    }, false);
 
+    // Update the seconds since alert
     setInterval(function() {
-        secondsElapsed++;
-
-        // Update the "it has been <> hours since" 
-        var timeSpan = document.getElementById("time-since-breach");
-        timeSpan.innerHTML = new Date(secondsElapsed * 1000).toISOString().substr(11, 8);
+        secondsSinceAlert++;
+        setTime();
     }, 1000);
-}
+
+    setActive();
+    flashAlert(false);
+});
 
 function reset() {
+    setActive();
+
+    $.post( deviceURL + "reset/?access_token=" + accessToken, function() {
+        var alertDiv = document.getElementById("alert-text");
+        alertDiv.innerText = "No active alerts.";
+        alertDiv.classList.remove("flashing");
+        
+        secondsSinceAlert = 0;
+        setTime();
+    });      
+}
+
+function setTime() {
+    // Update the "it has been <> hours since" 
+    var timeSpan = document.getElementById("time-since-breach");
+    timeSpan.innerHTML = new Date(secondsSinceAlert * 1000).toISOString().substr(11, 8);    
+}
+
+function setActive() {
+    // Set "system is active" text
     var activeSpan = document.getElementById("is-active");
     activeSpan.innerHTML = "active";
+}
 
+function alert() {
+    // No time has passed since the last alert!
+    secondsSinceAlert = 0;
+    setTime();
+
+    // Yeah dude start warning everyone!
     var alertDiv = document.getElementById("alert-text");
-    alertDiv.innerText = "No active alerts.";
+    alertDiv.innerText = "ALERT! Breach detected!";
+    flashAlert(true);
+}
 
-    secondsElapsed = 0;
+function flashAlert(addFlash) {
+    var alertDiv = document.getElementById("alert-text");
+
+    if (addFlash) {
+        alertDiv.classList.add("flashing");
+    }
+    else {
+        alertDiv.classList.remove("flashing");
+    }
 }
