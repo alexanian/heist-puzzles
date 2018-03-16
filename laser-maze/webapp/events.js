29990f6d1ccecf58eefd57fa2e24ac02049dadc7
tessa <tessa@zymergen.com>
2018-03-15 20:59:53 -0700

    Frontend exists, shows time elapsed

--- laser-maze/webapp/events.js
+++ laser-maze/webapp/events.js
@@ -1,17 +1,39 @@
 // Particle event detection, borrowed from this tutorial:
 // https://community.particle.io/t/tutorial-getting-started-with-spark-publish/3422
 // accessToken should be imported from another (secret) file
-function start() {
-    var eventSource = new EventSource("https://api.spark.io/v1/devices/events/?access_token=" + accessToken);
+var secondsElapsed = 0;
 
-    eventSource.addEventListener('alert', function(e) {
-        var parsedData = JSON.parse(e.data);
-        var tempSpan = document.getElementById("status");
-        tempSpan.innerHTML = "Status: " + parsedData.data;
-        tempSpan.style.fontSize = "28px";
+function start() {
+    var activeSpan = document.getElementById("is-active");
+    activeSpan.innerHTML = "active";
+
+    // var eventSource = new EventSource("https://api.spark.io/v1/devices/events/?access_token=" + accessToken);
+    // eventSource.addEventListener('alert', function(e) {
+    //     var parsedData = JSON.parse(e.data);
+    //     var tempSpan = document.getElementById("status");
+    //     tempSpan.innerHTML = "Status: " + parsedData.data;
+    //     tempSpan.style.fontSize = "28px";
         
-        var tsSpan = document.getElementById("ts");
-        tsSpan.innerHTML = "At timestamp " + parsedData.published_at;
-        tsSpan.style.fontSize = "9px";
-    }, false);
+    //     var tsSpan = document.getElementById("ts");
+    //     tsSpan.innerHTML = "At timestamp " + parsedData.published_at;
+    //     tsSpan.style.fontSize = "9px";
+    // }, false);
+
+    setInterval(function() {
+        secondsElapsed++;
+
+        // Update the "it has been <> hours since" 
+        var timeSpan = document.getElementById("time-since-breach");
+        timeSpan.innerHTML = new Date(secondsElapsed * 1000).toISOString().substr(11, 8);
+    }, 1000);
+}
+
+function reset() {
+    var activeSpan = document.getElementById("is-active");
+    activeSpan.innerHTML = "active";
+
+    var alertDiv = document.getElementById("alert-text");
+    alertDiv.innerText = "No active alerts.";
+
+    secondsElapsed = 0;
 }
